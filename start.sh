34a6b3ad446edb12f08a5326fb7aaf9d2606fc48
Abigail <riquelmyalternativo@gmail.com>
2026-03-15 09:12:52 -0300

    changes in start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 uvicorn main:app --host 0.0.0.0 --port 8000 &
 
-streamlit run cli.py --server.port 8501 --server.address 0.0.0.0
+streamlit run cli.py --server.port $PORT --server.address 0.0.0.0
